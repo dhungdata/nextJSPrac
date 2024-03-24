dccb05f50c246a3bc5467b48c26e23e478e0d128
dhungdata <dhungdata+hitbug@gmail.com>
2024-03-23 19:27:54 -0700

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function CustomersPages(){
+    return <p>Customers Pages</p>
+}
---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        {/*landing page (page.tsx)*/}
+        {children}
+      </body>
+    </html>
+  );
+}
